cb1fbb2330bbd57758fc8221cba1175cbc9a5979
priyanshi-rathore0 <applatus.priyanshi@gmail.com>
2025-03-01 20:46:08 +0530

    -

--- routes/payoutRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { initiatePayout } = require("../controllers/payoutController");
-
-const router = express.Router();
-
-// POST /api/payouts
-router.post("/send", initiatePayout);
-
-module.exports = router;
